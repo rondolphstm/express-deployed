933d0586578bac3fa41255c8541ae8ad1be30bb1
rondolphstm <rondolphtst.m@gmail.com>
2022-08-22 15:35:02 -0400

    added indigo the great

--- functions/index.js
+++ functions/index.js
@@ -6,18 +6,18 @@ import cors from 'cors'
 import {getDogs, testApi} from './src/dogs.js';
 
 
-const app = express();// sets up expess app
-app.use(cors()); //tells it to allow cross-origin resource sharing
+const app = express(); // sets up expess app
+app.use(cors()); // tells it to allow cross-origin resource sharing
 app.use(express.json()); // allows posts with JSON bodies
 
 // now we setup some routes...
 app.get('/dogs', getDogs);
 
-app.get('/test', testApp);
+app.get('/test', testApi);
 
-//instead of app.lissten(PORT)...
+// instead of app.lissten(PORT)...
 
-//we need to 1. create a cloud function, and 2. point it to "app"
+// we need to 1. create a cloud function, and 2. point it to "app"
 export const api = Functions.https.onRequest(app);
 
 
--- functions/src/dogs.js
+++ functions/src/dogs.js
@@ -1,9 +1,11 @@
-export function getDogs(req,res){
-    res.send([{name: 'ryder', age: 4, breed: 'Dumbass', gender: 'mn'},
-    {name: 'Duke', age: 2, breed: 'rotweiler', gender 'm'}
-])
+export function getDogs(req, res) {
+  res.send([
+    { name: "ryder", age: 4, breed: "Dumbass", gender: "mn" },
+    { name: "Duke", age: 2, breed: "rotweiler", gender: "m" },
+    { name: "indigo", age: 10, bredd: "great dane / lab mix", gender: "mn" },
+  ])
 }
 
-export function testApi(req,res){
-    res.send({success: true, message: 'API is working!'})
+export function testApi(req, res) {
+  res.send({ success: true, message: "API is working!" })
 }
